d0712427879609f2964b28c0b38680e380765b59
User1 <dddd30065@gmail.com>
2024-11-07 19:41:56 +0300

    Initial commit:Lesson41_Football_UpList

--- src/Football/Main.java
+++ src/Football/Main.java
@@ -2,25 +2,31 @@ package Football;
 
 public class Main {
     public static void main(String[] args) {
-        Player player1 = Player.addPlayer();
-        assert player1 != null;
-        Player player2 = Player.addPlayer();
-        assert player2 != null;
-        Player player3 = Player.addPlayer();
-        assert player3 != null;
-        Player player4 = Player.addPlayer();
-        assert player4 != null;
-        Player player5 = Player.addPlayer();
-        assert player5 != null;
-        System.out.println(player1.getStamina());
-        player1.run();
-        System.out.println(player1.getStamina());
-        System.out.println(player1.getName());
-        Player.info();
-        Player player6 = Player.addPlayer();
-        assert player6 != null;
-        Player.info();
-        System.out.println(player6.getStamina());
-        player6.run();
+        Game game = new Game();
+        System.out.println(game.getPlayers());
+        game.info();
+
+        game.addPlayer("Василий");
+        game.addPlayer("Алексей");
+        game.addPlayer("Николай");
+
+
+        System.out.println(game.getPlayers());
+        game.addPlayer("Михаил");
+        game.addPlayer("Александр");
+        game.addPlayer("Василий");
+        System.out.println(game.getPlayers());
+
+
+        game.addPlayer("Борис");
+
+        game.getPlayers().get(2).runFullStamina();
+        game.info();
+
+        game.getPlayers().get(4).runFullStamina();
+        game.info();
+
+        game.checkName("Александр");
+        game.checkName("Николас");
     }
 }
